5934385fce61a31bac463a60e00a1b2970dcff27
John Shaffer <john@insilica.co>
2023-01-11 16:26:32 -0600

    First implementation

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "srvc-project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    srvc.url = "github:insilica/rs-srvc";
+  };
+  outputs = { self, nixpkgs, flake-utils, srvc, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; };
+      let
+        srvc-project = stdenv.mkDerivation {
+          name = "srvc-project";
+          src = ./.;
+
+          installPhase = ''
+            mkdir -p $out
+          '';
+        };
+      in {
+        packages = {
+          inherit srvc-project;
+          default = srvc-project;
+        };
+        devShells.default =
+          mkShell { buildInputs = [ srvc.packages.${system}.default ]; };
+      });
+}
